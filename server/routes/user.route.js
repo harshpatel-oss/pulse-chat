7efd45b164437d30c8450d9cf44161aa3fee6957
Harsh Patel <harshpatel15505@gmail.com>
2026-06-09 07:58:43 +0530

    fix(routes): update import statements to include file extensions and improve formatting

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { checkAuth,login, signup, updateprofile } from "../controllers/user.controller";
-import { protectRoute } from "../middlewares/auth.middleware";
+import { checkAuth,login, signup, updateprofile } from "../controllers/user.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/signup",signup);
-userRouter.post("/login",login);
-userRouter.post("/update-profile",protectRoute,updateprofile);
-userRouter.post("/check",protectRoute,checkAuth);
-
+userRouter.post("/signup", signup);
+userRouter.post("/login", login);
+userRouter.put("/update-profile", protectRoute, updateprofile);
+userRouter.get("/check", protectRoute, checkAuth);
 
 export default userRouter;
